0d46c408565fdf309d6aaf6489ca6da3d774d966
adrian-codethink <adrianscarlett@codethink.co.uk>
2024-10-04 08:58:04 +0100

    Create flake.nix
    modified stateVersion, ssh key, hostName

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.04";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, disko, ... }:
+    let
+      # TODO: Adjust these values to your needs
+      system = "x86_64-linux";
+      hostName = "nixos-vm";
+      rootAuthorizedKeys = [
+        # This user can ssh using `ssh root@<ip>`
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6OgrgddyY05gzVggok2riToZffOqEqIouS39WoI4Jt"
+      ];
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./disk-config.nix
+          disko.nixosModules.disko
+          ({ pkgs, ... }: {
+            boot.loader = {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            };
+            networking = { inherit hostName; };
+            services.openssh.enable = true;
+            environment.systemPackages = with pkgs; [
+              git
+              neovim
+            ];
+
+            users.users.root.openssh.authorizedKeys.keys = rootAuthorizedKeys;
+
+            system.stateVersion = "24.04";
+          })
+        ];
+      };
+    };
+}
